17c90b912504da435db9416e3c5aa0090b2b7618
aslepakurov <a.slepakurov@gmail.com>
2021-03-10 18:50:36 +0200

    ready test

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 count=$(curl localhost:9098/payload -H "Content-type: application/json" -s -d '{"credit": "4111111111111111"}' | grep -c "tok_sat")
 exit_code=$((count ^ 1))
 exit $exit_code
