449e2688b6b1e2e5f97a35e63ddccabb3c172596
aslepakurov <a.slepakurov@gmail.com>
2021-03-17 18:05:54 +0200

    All good now

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ async def health():
     return "OK"
 
 
-@app.post("/invalid")
+@app.post("/payload")
 async def payload(request: Request):
     body_str = await request.body()
     return str(body_str.decode("UTF-8"))
