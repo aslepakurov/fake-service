7745b15228f2f01ea8f672b7ab2efe0dadfe34f3
aslepakurov <a.slepakurov@gmail.com>
2021-03-17 18:41:58 +0200

    Introduce new functionality

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ async def health():
     return "OK"
 
 
-@app.post("/payload")
+@app.post("/payload1")
 async def payload(request: Request):
+    print("Some additional funtionality")
     body_str = await request.body()
     return str(body_str.decode("UTF-8"))
